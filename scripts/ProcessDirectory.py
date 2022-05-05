46088cd29094ba1f813fa46aed69ad6307f6ca65
Stephan <stephan.hirth@tu-dresden.de>
2022-05-05 11:01:30 +0200

    Added some functions to convert data when tool is not able to produce detailed outputs e.g. minutely Then only at the time points provided by the tool data is compared.

--- scripts/ProcessDirectory.py
+++ scripts/ProcessDirectory.py
@@ -50,7 +50,7 @@ def listsEqual(list1, list2):
 	return True
 
 
-def evaluateVariableResults(variable, timeColumn, refData, testData, start, end, weightFactors):
+def evaluateVariableResults(variable, timeColumnRef, timeColumnData, refData, testData, start, end, weightFactors):
 	"""
 	Performance difference calculation between variable data sets.
 	
@@ -61,19 +61,47 @@ def evaluateVariableResults(variable, timeColumn, refData, testData, start, end,
 	printNotification("    {}".format(variable))
 	cr = CaseResults()
 	
+
+	# Check if time columns are equal. Some tools cannot produce output in under hourly mannor.
+	# For this we are nice and try to convert our reference results.
+	if not listsEqual(timeColumnData, timeColumnRef):
+		printWarning(f"        Mismatching time columns in Data set file and reference data set.")
+		printWarning(f"        Trying to convert reference data set.")
+		
+		newRefData = []
+		
+		for i in range(len(timeColumnData)):
+			index = timeColumnRef.index(timeColumnData[i])
+			newRefData.append(refData[index])
+		
+		# time column data from tool data set is now set for reference data set
+		timeColumnRef = timeColumnData
+		refData = newRefData
+		
+		
+	# We first convert our data to pandas
 	try:
+		pdTime = pd.DataFrame(data=pd.date_range(start="2018-01-01", periods=len(timeColumnRef), freq="H"), index=timeColumnRef, columns=["Date and Time"])
+		pdData = pd.DataFrame(data=testData, index=timeColumnData, columns=["Data"])
+		pdRef = pd.DataFrame(data=refData, index=timeColumnRef, columns=["Data"])
+	except ValueError as e:
+		printWarning(str(e))
+		printWarning(f"        Could not convert given data of file to pandas dataframe.")
+		cr.ErrorCode = -15
+		return cr
 		
-		# We first convert our data to pandas
-		pdTime = pd.DataFrame(data=pd.date_range(start="2018-01-01", periods=len(timeColumn), freq="H"), index=timeColumn, columns=["Date and Time"])
-		pdData = pd.DataFrame(data=testData, index=timeColumn, columns=["Data"])
-		pdRef = pd.DataFrame(data=refData, index=timeColumn, columns=["Data"])
-		
-		# We only use data between out start and end point
-		pdTime = pdTime.loc[start:end]
-		pdData = pdData.loc[start:end]
-		pdRef = pdRef.loc[start:end]
-		
-		# we evaluate the results		
+	
+	# We only use data between out start and end point
+	pdTime = pdTime.loc[start:end]
+	pdData = pdData.loc[start:end]
+	pdRef = pdRef.loc[start:end]
+	
+	# initialize all statistical methods in cr.norms
+	for key in weightFactors:
+		cr.norms[key] = -99
+	
+	try:	
+		# we evaluate the results
 		cr.norms['Maximum'] = sf.function_Maximum(pdRef["Data"], pdData["Data"], pdTime["Date and Time"])
 		cr.norms['Minimum'] = sf.function_Minimum(pdRef["Data"], pdData["Data"], pdTime["Date and Time"])
 		cr.norms['Average'] = sf.function_Average(pdRef["Data"], pdData["Data"], pdTime["Date and Time"])
@@ -91,25 +119,25 @@ def evaluateVariableResults(variable, timeColumn, refData, testData, start, end,
 		cr.norms['std dev'] = sf.function_std_dev(pdRef["Data"], pdData["Data"], pdTime["Date and Time"])
 		
 		
-	except IOError as e:
-		print(e)
-		print("Cannot create statistical evaluation for variable %1".format(variable))
-		exit(1)		
+	except (RuntimeError, RuntimeWarning) as e:
+		printError(f"        {str(e)}")
+		printError(f"        Cannot calculate statistical evaluation for variable '{variable}'")
+			
 
 	# TODO : Wichtung
 	if(abs(cr.norms['Average']) < 1e-4):
 		cr.score = 0		# prevent division by zero error
 	else:
-		cr.score = ( weightFactors.get('CVRMSE', 0) * ( 100.0 - cr.norms['CVRMSE'] ) +  		# in %
+		cr.score = ( weightFactors.get('CVRMSE', 0) * ( 100.0 - cr.norms['CVRMSE'] ) +  				# in %
 			     weightFactors.get('Daily Amplitude CVRMSE', 0) * ( 100.0 - cr.norms['Daily Amplitude CVRMSE'] ) + 	# in % 
 			     weightFactors.get('MBE', 0) * ( 100.0 - 100.0*cr.norms['MBE'] / cr.norms['Average']) + 
-			     weightFactors.get('RMSEIQR', 0) * ( 100.0 - cr.norms['RMSEIQR'] ) + 		# in %
+			     weightFactors.get('RMSEIQR', 0) * ( 100.0 - cr.norms['RMSEIQR'] ) + 				# in %
 			     weightFactors.get('MSE', 0) * ( 100.0 - 100*cr.norms['MSE'] / cr.norms['Average']) + 
-			     weightFactors.get('NMBE', 0) * ( 100.0 - cr.norms['NMBE'] ) + 
-			     weightFactors.get('NRMSE', 0) * ( 100.0 - cr.norms['NRMSE'] ) + 
+			     weightFactors.get('NMBE', 0) * ( 100.0 - cr.norms['NMBE'] ) + 					# in %
+			     weightFactors.get('NRMSE', 0) * ( 100.0 - cr.norms['NRMSE'] ) + 					# in %
 			     weightFactors.get('RMSE', 0) * ( 100.0 - 100.0*cr.norms['RMSE'] / cr.norms['Average']) + 
 			     weightFactors.get('RMSLE', 0) * ( 100.0 - cr.norms['RMSLE'] / cr.norms['Average']) + 
-			     weightFactors.get('R squared coeff determination', 0)*( 100.0 - cr.norms['R squared coeff determination'] / cr.norms['Average']) + 
+			     weightFactors.get('R squared coeff determination', 0)*( cr.norms['R squared coeff determination'] ) + # in %
 			     weightFactors.get('std dev', 0)*( 100.0 - cr.norms['std dev'] / cr.norms['Average']) ) / weightFactors['Sum'] 
 	
 	# scoring caluclation --> >95% : Gold | >90% : Silver | >80% : Bronze
@@ -146,16 +174,16 @@ def processDirectory(path, weightFactors):
 	tsvPath = os.path.join(path, RESULTS_SUBDIRNAME)
 
 	if not os.path.exists(tsvPath):
-		printError("Missing test result directory '{}'.".format(tsvPath))
+		printError("    Missing test result directory '{}'.".format(tsvPath))
 		return None # None indicates entirely invalid/missing test data.
 	
 
 	tsvFiles = [o for o in os.listdir(tsvPath) if o.endswith("tsv")]
 	if not "Reference.tsv" in tsvFiles:
-		printError("Missing 'Reference.tsv' file.")
+		printError("    Missing 'Reference.tsv' file.")
 		return None
 	if not "EvaluationPeriods.tsv" in tsvFiles:
-		printError("Missing 'EvaluationPeriods.tsv' file.")
+		printError("    Missing 'EvaluationPeriods.tsv' file.")
 		return None
 	tsvFiles = sorted(tsvFiles)
 	
@@ -163,7 +191,7 @@ def processDirectory(path, weightFactors):
 	evalData = TSVContainer()
 	evalData.readAsStrings(os.path.join(tsvPath, "EvaluationPeriods.tsv"))
 	if True in evalData.emptyColumn:
-		printError("'EvaluationPeriods.tsv' contains empty columns.")
+		printError("    'EvaluationPeriods.tsv' contains empty columns.")
 		return None
 
 	
@@ -171,10 +199,10 @@ def processDirectory(path, weightFactors):
 	refData = TSVContainer()
 	refData.readAsStrings(os.path.join(tsvPath, "Reference.tsv") )
 	if True in refData.emptyColumn:
-		printError("'Reference.tsv' contains empty columns.")
+		printError("    'Reference.tsv' contains empty columns.")
 		return None
 	if not refData.convert2Double():
-		printError("'Reference.tsv' contains invalid numbers.")
+		printError("    'Reference.tsv' contains invalid numbers.")
 		return None
 	
 	# extract variable names
@@ -187,7 +215,7 @@ def processDirectory(path, weightFactors):
 		if p == -1:
 			p = v.find("[")
 		if p == -1:
-			print("Missing unit in header label '{}' of 'Reference.tsv'".format(v))
+			printError("    Missing unit in header label '{}' of 'Reference.tsv'".format(v))
 			return None
 		v = v[0:p].strip()
 		variables.append(v)
@@ -209,34 +237,25 @@ def processDirectory(path, weightFactors):
 		if dataFile.startswith("EvaluationPeriods"):
 			continue		
 		
+		printNotification("\n-------------------------------------------------------\n")
 		printNotification("Reading '{}'.".format(dataFile))
 		toolID = dataFile[0:-4] # strip tsv
 		tsv = TSVContainer()
 		tsv.readAsStrings(os.path.join(tsvPath, dataFile))
 		if True in tsv.emptyColumn:
-			printError("'{}' contains empty columns, skipped.".format(dataFile))
+			printError("    '{}' contains empty columns. Skipped.".format(dataFile))
 			appendErrorResults(tsvData, testCaseName, toolID, -10, variables)
 			continue
-		# check if header line is exactly correct
-		#if not listsEqual(refData.headers, tsv.headers):
-			#printError("'{}''s header doesn't match header of 'Reference.tsv'".format(dataFile))
-			#appendErrorResults(tsvData, testCaseName, toolID, -9, variables)
-			#continue
 		
 		# if not all data is provieded by a tool we only want to skip the specific variable
 		for header in tsv.headers:
 			if header not in refData.headers:
-				printError("'{}''s header '{}' is not part of the reference header".format(dataFile, header))
+				printError(f"    '{dataFile}'s header '{header}' is not part of the reference header. Skipped.")
 				continue
 		
-		# number of cols
-		#if len(refData.data) != len(tsv.data):
-			#printError("'{}''s mismatching number of columns in file compared to 'Reference.tsv'".format(dataFile))
-			#appendErrorResults(tsvData, testCaseName, toolID, -8, variables)
-			#continue
-
+		# Check if only valid numbers are in file
 		if not tsv.convert2Double():
-			printError("Data file contains invalid numbers.")
+			printError("    Data file contains invalid numbers. Skipped.")
 			appendErrorResults(tsvData, testCaseName, toolID, -7, variables)
 			continue
 		
@@ -246,41 +265,38 @@ def processDirectory(path, weightFactors):
 			# we provide time column, reference data column and value column, also parameter set for norm calculation
 			# we get a variable-specific score stored in CaseResults object
 			if not rawVariables[i] in tsv.headers:
-				printError("'{}''s does not contain the variable {} and will be skipped.".format(dataFile, variables[i]))
+				printError("    '{}'s does not contain the variable {}. Skipped".format(dataFile, variables[i]))
 				continue
 			
 			# check if data even exists
 			if i > len(tsv.data) :
-				printError("'{}''s columns exceed number of columns of 'Reference.tsv'".format(dataFile))
+				printError("    '{}'s columns exceed number of columns of 'Reference.tsv'".format(dataFile))
 				appendErrorResults(tsvData, testCaseName, toolID, -11, variables)
 				break					
-
-			# number of rows
-			if len(refData.data[i]) != len(tsv.data[i]):
-				printError("'{}''s mismatching number of rows ({}) in file compared to 'Reference.tsv' ({})".format(dataFile, len(tsv.data[i]), len(refData.data[i]) ) )
-				appendErrorResults(tsvData, testCaseName, toolID, -12, variables)
-				continue
 			
 
 			if not variables[i] in evaluationVariables:
-				printError("'EvaluationPeriods.tsv' does not contain the variable '{}'".format(variables[i]))
+				printError("    'EvaluationPeriods.tsv' does not contain the variable '{}'. Skipped.".format(variables[i]))
 				continue
 			
 			for j in range(len(evaluationVariables)):
 				if variables[i] == evaluationVariables[j]:
-					start = int(evalData.data[1][j])
-					end = int(evalData.data[2][j])
+					start = float(evalData.data[1][j])
+					end = float(evalData.data[2][j])
 					break
 			
 			if end < start:
-				printError("Evaluation End Point ({}) has to be after start point ({}).".format(end, start))
+				printError("    Evaluation End Point ({}) has to be after start point ({}). Skipped.".format(end, start))
 				continue
 						
-			if end > len(refData.data[0]):
-				printError("Evaluation End Point ({}) is bigger then row number of reference results.".format(end, len(refData.data[0])))
+			if end > refData.data[0][-1]:
+				printError("    Evaluation End Point ({}) is bigger then last time stamp of reference results ({}). Skipped.".format(end, refData.data[0][-1]))
+				continue
+			if start < refData.data[0][0]:
+				printError("    Evaluation Start Point ({}) is smaller then first time stamp of reference results ({}). Skipped.".format(end, refData.data[0][0]))
 				continue				
 			
-			cr = evaluateVariableResults(variables[i], refData.data[0], refData.data[i+1], tsv.data[i+1], start, end, weightFactors)
+			cr = evaluateVariableResults(variables[i], refData.data[0], tsv.data[0], refData.data[i+1], tsv.data[i+1], start, end, weightFactors)
 			cr.TestCase = testCaseName
 			cr.ToolID = toolID
 			cr.Variable = variables[i]
